6c86c226b1cd1e9e8d38aed2e83b172dc3ce52c8
Mark Mcloughlin <mark25mky@gmail.com>
2024-04-11 15:35:07 +0000

    added button elements js

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const startButton = document.getElementById('start-button');
+const nextButton = document.getElementById('next');
+const questionContainer = document.getElementById('question');
+const answerButtons = document.getElementById('answer-button');
