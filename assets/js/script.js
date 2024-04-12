d80c8dd962ed0fc71f638b300e5083198666153a
Mark Mcloughlin <mark25mky@gmail.com>
2024-04-12 14:33:12 +0000

    added css to hide questions initially

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
 const startButton = document.getElementById('start-button');
+const gameArea = document.querySelector('.game-area');
+const instructionsArea = document.querySelector('.instructions-area');
 const nextButton = document.getElementById('next');
 const questionContainer = document.getElementById('question');
 const answerButtons = document.getElementById('answer-button');
+
+startButton.addEventListener('click', () => {
+
+    instructionsArea.style.display = 'none';
+
+    gameArea.style.display = 'block';
+});
