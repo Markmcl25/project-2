5f2b91295306afbd0e251e20fe7269c69b8141dd
Mark Mcloughlin <mark25mky@gmail.com>
2024-04-12 17:11:20 +0000

    fixed the create username js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,26 +3,23 @@ const gameArea = document.querySelector('.game-area');
 const instructionsArea = document.querySelector('.instructions-area');
 
 // Event listener for start button
-
-startButtons.addEventListener('click', () => {
-
+startButton.addEventListener('click', () => {
     instructionsArea.style.display = 'none';
-
     gameArea.style.display = 'block';
 });
 
 // Event listener for username form submission
-document.getElementById("username-form").addEventListener("submit", function(event) {
+document.querySelector(".create-username form").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevent form submission
 
     // Get username value
     var username = document.getElementById("username").value;
-    
+
     // Display username on the screen
     var displayUsername = document.getElementById("display-username");
     displayUsername.textContent = "Username: " + username;
-    
+
     // Hide create username section
-    var createUsernameSection = document.getElementById("create-username-section");
+    var createUsernameSection = document.querySelector(".create-username");
     createUsernameSection.style.display = "none";
 });
