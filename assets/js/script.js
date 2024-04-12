8ca76fb06467a1c89ed02b7fc4e74e24e606d5ef
Mark Mcloughlin <mark25mky@gmail.com>
2024-04-12 16:52:50 +0000

    added js for username creation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,28 @@
 const startButton = document.getElementById('start-button');
 const gameArea = document.querySelector('.game-area');
 const instructionsArea = document.querySelector('.instructions-area');
-const nextButton = document.getElementById('next');
-const questionContainer = document.getElementById('question');
-const answerButtons = document.getElementById('answer-button');
 
 // Event listener for start button
 
-startButton.addEventListener('click', () => {
+startButtons.addEventListener('click', () => {
 
     instructionsArea.style.display = 'none';
 
     gameArea.style.display = 'block';
+});
+
+// Event listener for username form submission
+document.getElementById("username-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get username value
+    var username = document.getElementById("username").value;
+    
+    // Display username on the screen
+    var displayUsername = document.getElementById("display-username");
+    displayUsername.textContent = "Username: " + username;
+    
+    // Hide create username section
+    var createUsernameSection = document.getElementById("create-username-section");
+    createUsernameSection.style.display = "none";
 });
