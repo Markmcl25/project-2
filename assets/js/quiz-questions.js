129886b2d2b927114b66ecf21f9a09fda3fc52b3
Mark Mcloughlin <mark25mky@gmail.com>
2024-04-14 12:31:14 +0000

    fixed questions on seperate js sheet

--- assets/js/quiz-questions.js
+++ assets/js/quiz-questions.js
@@ -1,166 +1,197 @@
-// Quiz data
-
 const quizQuestions = [
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
 
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
+{
+    question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            {},
+            {},
+            {},
+            {},
+        ]
+},
 
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
+ {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
 
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
 
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
 
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
 
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
 
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
 
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
 
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
 
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
 
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
 
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
 
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
-
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
-
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
-
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
-
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
-
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
-
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
-
-question: "Life of Pi, was a famous book by which writer?",
-answers: [
-    {},
-    {},
-    {},
-    {},
-],
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
+    {
+        question: "Life of Pi, was a famous book by which writer?",
+        answers: [
+            { text: "Cormac McCarthy" },
+            { text: "J.K. Rowling" },
+            { text: "Yann Martel", correct: true },
+            { text: "Stephen King" }
+        ]
+    },
   
 
 
