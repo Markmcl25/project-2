3045506af12df78e1f1c60843071c5ce9cdf178b
Mark Mcloughlin <mark25mky@gmail.com>
2024-04-15 13:11:33 +0000

    added 2nd question js

--- assets/js/quiz-questions.js
+++ assets/js/quiz-questions.js
@@ -10,12 +10,12 @@ const quizQuestions = [
     },
 
 {
-    question: "Life of Pi, was a famous book by which writer?",
+    question: "You're 4th place right now in a race. What place will you be in when you pass the person in 3rd place?",
         answers: [
-            {},
-            {},
-            {},
-            {},
+            {text: "1st"},
+            {text: "2nd"},
+            {text: "3rd", correct: true},
+            {text: "None of the above"},
         ]
 },
 
